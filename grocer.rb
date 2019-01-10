73fe7a8a1a0e0b1940dd4ad057702ea3064c8405
Sim Greenbaum <gbs4ever2@gmail.com>
2019-01-10 05:10:42 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,26 +1,38 @@
 require 'pry'
 
 def consolidate_cart(cart)
-  i=1
-  hash= {}
-  cart.each do |item|
-    item.each do |attributes|
-      attributes.each do |k|
-    price_data=attributes[1]
-hash[k][price_data] = ":count => #{i}"
-  binding.pry
 
-        i+=1
+      hash= {}
+      cart.each do |item|
+        item.each do |key, value|
+          if hash.has_key?(key) == true
+                hash[key][:count]+=1
+              else
+               hash[key]=value
+              hash[key][:count]=1
 
-      end
-    end
+
+           end
+        end
   end
+  hash
 end
 
 def apply_coupons(cart, coupons)
-  # code here
-end
+  # we need to get all the key value pair from the coupon to update the cart
+  # some hints  we might need sting manuplation for the new
+coupons.each do|coupon|
+  coupon.each do |k,v|
+cart.each do |item,price|
+if v == item
+  cart
 
+
+
+end
+end
+end
+end
 def apply_clearance(cart)
   # code here
 end
